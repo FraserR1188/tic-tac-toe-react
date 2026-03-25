f3fa2b494018f2e8abe2a71f6dd907afd8e08f54
Robbie Fraser <fraserrobbie2@gmail.com>
2026-03-25 12:51:22 +0000

    add list for players

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-
 function App() {
-  
-
   return (
-    <p>Comming soon ...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+          </li>
+          <li>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+          </li>
+          <li>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
